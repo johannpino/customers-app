91a0e33be1ba4d7ee6018254a4b41e75011b4f79
johannpino <johannpino@gmail.com>
2018-12-06 17:42:13 -0300

    Integramos redux-form

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {reduxForm, Field} from 'redux-form'
+import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
 const CustomerEdit = ({name,dni,age}) => {
     return (
-        <div>
+  
+  <div>
             <h2>Edicion del cliente</h2>
-            <h3>Nombre:{name} / Dni:{dni} / Edad:{age} </h3>
+            <form>
+                <div>
+                    <label htmlFor="name" >Nombre</label>
+                    <Field name="name" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="dni">Dni</label>
+                    <Field name="dni" component="input" type="text"></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Edad</label>
+                    <Field name="age" component="input" type="number"></Field>
+                </div>
+            </form>
         </div>
     );
 };
 
 CustomerEdit.propTypes = {
-    name:PropTypes.string.isRequired,
-    dni:PropTypes.string.isRequired,
-    age:PropTypes.number.isRequired,
+    name:PropTypes.string,
+    dni:PropTypes.string,
+    age:PropTypes.number,
 };
 
-export default CustomerEdit;
+
+const CustomerEditForm = reduxForm({form: 'CustomerEdit'})(CustomerEdit)
+
+console.log(CustomerEditForm)
+
+
+export default setPropsAsInitial(CustomerEditForm)
